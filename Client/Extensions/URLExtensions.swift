6d44564d7840a8996d4d6fa9ae1d2de701fb0e6c
Burak Üstün <burak@neeva.co>
2022-05-19 20:00:04 +0300

    Fix license text

--- Client/Extensions/URLExtensions.swift
+++ Client/Extensions/URLExtensions.swift
@@ -1,10 +1,6 @@
-//
-//  URLExtensions.swift
-//  Client
-//
-//  Created by Burak Üstün on 31.03.2022.
-//  Copyright © 2022 Neeva. All rights reserved.
-//
+// Copyright 2022 Neeva Inc. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
 
 import Foundation
 import Shared
