b326ee7b653a70782c3e7c95305e86ed15d0fe62
Edward Luo <edward@neeva.co>
2022-07-07 11:02:26 -0700

    Add set behaviour to get logging attributes (#3980)
    * add set
    
    * format
    
    * sort cases alphabetically
    
    * remove 30 min comment
    
    * remove get session UUID
    
    * Revert "remove get session UUID"
    
    This reverts commit 29fb85e7426819f5a388d4829663c19456a0a139.

--- Client/EnvironmentHelper.swift
+++ Client/EnvironmentHelper.swift
@@ -9,6 +9,27 @@ import Shared
 public class EnvironmentHelper {
     public static let shared = EnvironmentHelper()
 
+    public enum Category: Int, RawRepresentable, Hashable, CaseIterable {
+        case deviceName
+        case deviceOrientation
+        case deviceScreenSize
+        case deviceTheme
+        case firstRunPath
+        case isUserSignedIn
+        case previewQueryCount
+        case tabs
+        case tabGroups
+
+        static let firstRun: Categories = [
+            .isUserSignedIn,
+            .deviceTheme,
+            .deviceName,
+            .firstRunPath,
+            .previewQueryCount,
+        ]
+    }
+    public typealias Categories = Set<Category>
+
     public var env: ClientLogEnvironment {
         #if DEBUG
             return ClientLogEnvironment(rawValue: "Dev")!
@@ -53,106 +74,17 @@ public class EnvironmentHelper {
         return "\(UIScreen.main.bounds.width) x \(UIScreen.main.bounds.height)"
     }
 
-    public func getAttributes() -> [ClientLogCounterAttribute] {
-        var numOfNormalTabs = 0
-        var numOfIncognitoTabs = 0
-        var numOfArchivedTabs = 0
-        TabManager.all.forEach { tabManager in
-            numOfNormalTabs += tabManager.normalTabs.count
-            numOfIncognitoTabs += tabManager.incognitoTabs.count
-            numOfArchivedTabs += tabManager.archivedTabs.count
-        }
-
-        var numOfChildTabs = 0
-        var numOfTabGroups = 0
-        TabManager.all.forEach { tabManager in
-            // In the future, we may also want to log archived tab groups.
-            numOfTabGroups += tabManager.activeTabGroups.count
-            numOfChildTabs += tabManager.childTabs.count
-        }
-
-        // number of normal tabs opened
-        let normalTabsOpened = ClientLogCounterAttribute(
-            key: LogConfig.Attribute.NormalTabsOpened,
-            value: String(numOfNormalTabs))
-
-        // number of incognito tabs opened
-        let incongitoTabsOpened = ClientLogCounterAttribute(
-            key: LogConfig.Attribute.IncognitoTabsOpened,
-            value: String(numOfIncognitoTabs))
-
-        // number of archived tabs
-        let numArchivedTabsTotal = ClientLogCounterAttribute(
-            key: LogConfig.Attribute.NumberOfArchivedTabsTotal, value: String(numOfArchivedTabs))
-
-        // number of tab groups
-        let numTabGroupsTotal = ClientLogCounterAttribute(
-            key: LogConfig.Attribute.numTabGroupsTotal,
-            value: String(numOfTabGroups)
-        )
-
-        // number of tabs inside tab groups
-        let numChildTabsTotal = ClientLogCounterAttribute(
-            key: LogConfig.Attribute.numChildTabsTotal,
-            value: String(numOfChildTabs)
-        )
-
-        // user theme setting
-        let deviceTheme = ClientLogCounterAttribute(
-            key: LogConfig.Attribute.UserInterfaceStyle, value: self.themeStyle)
-
-        // orientation
-        let deviceOrientation = ClientLogCounterAttribute(
-            key: LogConfig.Attribute.DeviceOrientation, value: self.orientation)
-
-        // screensize
-        let deviceScreensSize = ClientLogCounterAttribute(
-            key: LogConfig.Attribute.DeviceScreenSize, value: self.screenSize)
-
-        // is user signed in
-        let isUserSignedIn = ClientLogCounterAttribute(
-            key: LogConfig.Attribute.isUserSignedIn,
-            value: String(NeevaUserInfo.shared.hasLoginCookie()))
-
-        let attributes = [
-            normalTabsOpened, incongitoTabsOpened, numTabGroupsTotal, numChildTabsTotal,
-            numArchivedTabsTotal,
-            deviceTheme, deviceOrientation, deviceScreensSize,
-            isUserSignedIn, getSessionUUID(),
+    // MARK: - Public Methods
+    public func getAttributes(
+        for categories: Categories = [
+            .tabs, .tabGroups, .deviceTheme, .deviceOrientation, .deviceScreenSize, .isUserSignedIn,
         ]
-
-        return attributes
+    ) -> [ClientLogCounterAttribute] {
+        categories.flatMap { getAttributes(category: $0) } + [getSessionUUID()]
     }
 
     public func getFirstRunAttributes() -> [ClientLogCounterAttribute] {
-        // is user signed in
-        let isUserSignedIn = ClientLogCounterAttribute(
-            key: LogConfig.Attribute.isUserSignedIn,
-            value: String(NeevaUserInfo.shared.hasLoginCookie()))
-
-        // user theme setting
-        let deviceTheme = ClientLogCounterAttribute(
-            key: LogConfig.Attribute.UserInterfaceStyle, value: self.themeStyle)
-
-        // device name
-        let deviceName = ClientLogCounterAttribute(
-            key: LogConfig.Attribute.DeviceName, value: NeevaConstants.deviceNameValue)
-
-        // first run path, option user selected on first run screen
-        let firstRunPath = ClientLogCounterAttribute(
-            key: LogConfig.Attribute.FirstRunPath, value: Defaults[.firstRunPath])
-
-        // count of preview mode query
-        let previewQueryCount = ClientLogCounterAttribute(
-            key: LogConfig.Attribute.PreviewModeQueryCount,
-            value: String(Defaults[.previewModeQueries].count))
-
-        let attributes = [
-            getSessionUUID(), isUserSignedIn, deviceTheme, deviceName, firstRunPath,
-            previewQueryCount,
-        ]
-
-        return attributes
+        getAttributes(for: Category.firstRun)
     }
 
     public func getSessionUUID() -> ClientLogCounterAttribute {
@@ -164,12 +96,118 @@ public class EnvironmentHelper {
 
         // session UUID that will rotate every 30 mins
         return ClientLogCounterAttribute(
-            key: LogConfig.Attribute.SessionUUID, value: Defaults[.sessionUUID])
+            key: LogConfig.Attribute.SessionUUID, value: Defaults[.sessionUUID]
+        )
     }
 
     public func getSessionUUIDv2() -> ClientLogCounterAttribute {
-        // session UUID that will rotate every 30 mins
         return ClientLogCounterAttribute(
-            key: LogConfig.Attribute.SessionUUIDv2, value: Defaults[.sessionUUIDv2])
+            key: LogConfig.Attribute.SessionUUIDv2, value: Defaults[.sessionUUIDv2]
+        )
+    }
+
+    // MARK: - Private Methods
+    private func getAttributes(category: Category) -> [ClientLogCounterAttribute] {
+        switch category {
+        case .tabs:
+            return getAttributesForTabCounts()
+        case .tabGroups:
+            return getAttributesForTabGroups()
+        case .isUserSignedIn:
+            return [
+                ClientLogCounterAttribute(
+                    key: LogConfig.Attribute.isUserSignedIn,
+                    value: String(NeevaUserInfo.shared.hasLoginCookie())
+                )
+            ]
+        case .deviceTheme:
+            return [
+                ClientLogCounterAttribute(
+                    key: LogConfig.Attribute.UserInterfaceStyle, value: self.themeStyle
+                )
+            ]
+        case .deviceName:
+            return [
+                ClientLogCounterAttribute(
+                    key: LogConfig.Attribute.DeviceName, value: NeevaConstants.deviceNameValue
+                )
+            ]
+        case .deviceOrientation:
+            return [
+                ClientLogCounterAttribute(
+                    key: LogConfig.Attribute.DeviceOrientation, value: self.orientation
+                )
+            ]
+        case .deviceScreenSize:
+            return [
+                ClientLogCounterAttribute(
+                    key: LogConfig.Attribute.DeviceScreenSize, value: self.screenSize
+                )
+            ]
+        case .firstRunPath:
+            return [
+                ClientLogCounterAttribute(
+                    key: LogConfig.Attribute.FirstRunPath, value: Defaults[.firstRunPath]
+                )
+            ]
+        case .previewQueryCount:
+            return [
+                ClientLogCounterAttribute(
+                    key: LogConfig.Attribute.PreviewModeQueryCount,
+                    value: String(Defaults[.previewModeQueries].count)
+                )
+            ]
+        }
+    }
+
+    private func getAttributesForTabCounts() -> [ClientLogCounterAttribute] {
+        // number of normal tabs opened
+        var numOfNormalTabs = 0
+        // number of incognito tabs opened
+        var numOfIncognitoTabs = 0
+        // number of archived tabs
+        var numOfArchivedTabs = 0
+        TabManager.all.forEach { tabManager in
+            numOfNormalTabs += tabManager.normalTabs.count
+            numOfIncognitoTabs += tabManager.incognitoTabs.count
+            numOfArchivedTabs += tabManager.archivedTabs.count
+        }
+
+        let normalTabsOpened = ClientLogCounterAttribute(
+            key: LogConfig.Attribute.NormalTabsOpened,
+            value: String(numOfNormalTabs)
+        )
+        let incongitoTabsOpened = ClientLogCounterAttribute(
+            key: LogConfig.Attribute.IncognitoTabsOpened,
+            value: String(numOfIncognitoTabs)
+        )
+        let numArchivedTabsTotal = ClientLogCounterAttribute(
+            key: LogConfig.Attribute.NumberOfArchivedTabsTotal, value: String(numOfArchivedTabs)
+        )
+
+        return [normalTabsOpened, incongitoTabsOpened, numArchivedTabsTotal]
+    }
+
+    private func getAttributesForTabGroups() -> [ClientLogCounterAttribute] {
+        // number of tabs inside tab groups
+        var numOfChildTabs = 0
+        // number of tab groups
+        var numOfTabGroups = 0
+        TabManager.all.forEach { tabManager in
+            // In the future, we may also want to log archived tab groups.
+            numOfTabGroups += tabManager.activeTabGroups.count
+            numOfChildTabs += tabManager.childTabs.count
+        }
+
+        let numTabGroupsTotal = ClientLogCounterAttribute(
+            key: LogConfig.Attribute.numTabGroupsTotal,
+            value: String(numOfTabGroups)
+        )
+        let numChildTabsTotal = ClientLogCounterAttribute(
+            key: LogConfig.Attribute.numChildTabsTotal,
+            value: String(numOfChildTabs)
+        )
+
+        return [numTabGroupsTotal, numChildTabsTotal]
     }
 }
