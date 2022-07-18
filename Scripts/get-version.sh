64f646bc659b4d0496523ddf60e3adbae3bde198
chung-lai <87332578+chung-lai@users.noreply.github.com>
2022-07-18 11:51:41 -0700

    delete common build scripts (#4046)
    * Make build script to start executing from neeva repo
    
    * delete common scripts, moved to another repo

--- Scripts/get-version.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-SCRIPTS_DIR=$NEEVA_REPO/client/browser/scripts/
-
-. $SCRIPTS_DIR/version-util.sh
-
-echo "Current version info:"
-echo "  MARKETING_VERSION = $(get_marketing_version)"
-echo "  CURRENT_PROJECT_VERSION = $(get_current_project_version)"
