50f6f8cb1339da332798e71228a9787cbee5a686
jonathangarelick <41495718+jonathangarelick@users.noreply.github.com>
2022-08-11 08:34:57 -0700

    Remove jsonDictionary from SessionData and SavedTab (#4193)

--- Client/Frontend/Browser/SessionData.swift
+++ Client/Frontend/Browser/SessionData.swift
@@ -56,16 +56,6 @@ class SessionData: NSObject, NSCoding {
     let queryLocations: [QueryForNavigation.Query.Location.RawValue?]
     let lastUsedTime: Timestamp
 
-    var jsonDictionary: [String: Any] {
-        return [
-            "currentPage": String(self.currentPage),
-            "urls": urls.map { $0.absoluteString },
-            "queries": typedQueries,
-            "suggestedQueries": suggestedQueries,
-            "lastUsedTime": String(self.lastUsedTime),
-        ]
-    }
-
     var currentUrl: URL? {
         // TODO: We should probably unwrap this if it is a session restore internal URL.
         let index = urls.count - 1 + currentPage
