66ace98a8f684a35e79438cc7cc2ec7f99c2a29b
chung-lai <87332578+chung-lai@users.noreply.github.com>
2022-08-19 15:54:26 -0700

    add localization script to help with export and import (#4257)

---
+++ Scripts/localization.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+while getopts "eih" option; do
+  case $option in
+    e) # export
+       if [ "$#" -lt 2 ]; then
+         echo "missing argument: export directory"
+         exit
+       fi
+       xcodebuild -exportLocalizations -localizationPath $2 -exportLanguage en
+       ;;
+    i) # import
+       if [ "$#" -lt 2 ]; then
+         echo "missing argument: import directory"
+         exit
+       fi
+       sed -i '' -e 's/fr-FR/fr/' $2/fr-FR/en.xliff
+       xcodebuild -importLocalizations -localizationPath $2/fr-FR/en.xliff
+       sed -i '' -e 's/de-DE/de/' $2/de-DE/en.xliff
+       xcodebuild -importLocalizations -localizationPath $2/de-DE/en.xliff
+       xcodebuild -importLocalizations -localizationPath $2/en-GB/en.xliff
+       echo "### Done importing all xliff files ###"
+       echo "Don't forget to commit and push the changes"
+       ;;
+    h) # help
+       echo "-e [export_dir]: export localization file to a specific directory"
+       echo "-i [import_dir]: import Smartling localization files from a specific directory"
+       exit
+  esac
+done
+
